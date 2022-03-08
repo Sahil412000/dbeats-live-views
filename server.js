fb86b60d2e91fe3e352e5e18e26492d41f4fb371
Sahil Punjabi <sahilpanjab123@gmail.com>
2022-03-08 17:46:19 +0530

    Add port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const port = 8000;
+const port = process.env.PORT || 8000;
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
