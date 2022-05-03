354e28a80005350a7354f5c6c543dfc729842e1d
Sahil412000 <sahilpanjabi123@gmail.com>
2022-05-03 14:53:31 +0530

    Revert Changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("https").createServer(app);
+const server = require("http").createServer(app);
 const port = process.env.PORT || 8000;
 const cors = require("cors");
 
