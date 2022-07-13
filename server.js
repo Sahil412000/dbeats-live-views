b162c2438d7e9b8ba33cc854614d6d39aa42676b
Supersapiens Devlab <78748112+supersapiensdevlab@users.noreply.github.com>
2022-07-14 00:09:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,46 +2,19 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const port = process.env.PORT || 8000;
-const cors = require("cors");
+const cors = require("cors");  
 
-// const corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-// };
-
-// app.use(cors(corsOptions));
+app.use(cors());
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://beta.dbeats.live",
-    allowedHeaders: ["my-custom-header"],
+    origin: "https://beta.mintflick.app", 
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
     credentials: true,
   },
 });
-
-//app.use(cors(corsOptions));
-
-// app.options('*', cors(corsOptions));
-
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: [
-//       "http://localhost:*",
-//       "https://beta.dbeats.live ",
-//       "https://dbeats.live ",
-//     ],
-//     transports: ["websocket", "polling"],
-//     credentials: true,
-//     allowedHeaders: ["Access-Control-Allow-Origin"],
-//     methods: ["GET", "POST", "PUT"],
-//   },
-//   allowEIO3: true,
-// });
-
-app.use(cors());
-
+ 
 app.get("/", (req, res) => {
   res.send("Live Viewer Working!");
 });
